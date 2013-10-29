b4226f9efcdfdaa3703e72ccf7a0e07a6817d134
roberto <roberto@golgi.local>
2013-10-29 19:01:49 +0100

    Initial Commit

---
+++ shredder/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  shredder
+//
+//  Created by roberto on 29/10/13.
+//  Copyright (c) 2013 roberto. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
